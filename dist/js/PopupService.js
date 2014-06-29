dce170eb5600506f4e2d9871ddc5c972a48ce265
Victor Aguilar <victor.aguilar@ciencias.unam.mx>
2014-06-29 14:08:30 -0500

    Added compatibility with loaders like requirejs

--- dist/js/PopupService.js
+++ dist/js/PopupService.js
@@ -1,71 +1,82 @@
-"use strict";
-
-angular.module('PopupService',['ui.bootstrap'])
-
-.factory('Popup',['$modal',function($modal)
-{	
-	var Popup = {};
-
-	var options = 
+(function(factory)
+{
+	if( typeof define === 'function' && define.amd)
 	{
-		template :
-		'<div class="modal-header">'+
-			'<h3 class="modal-title">&nbsp;{{title}}'+
-			'<a style="text-decoration: none; cursor: pointer;" '+
-				'class="glyphicon glyphicon-remove pull-right" '+
+		define(['ui-bootstrap'], factory);
+	}else{
+		factory();
+	}
+})(function()
+{
+	"use strict";
+
+	angular.module('PopupService',['ui.bootstrap'])
+
+	.factory('Popup',['$modal',function($modal)
+	{	
+		var Popup = {};
+
+		var options = 
+		{
+			template :
+			'<div class="modal-header">'+
+				'<h3 class="modal-title">&nbsp;{{title}}'+
+				'<a style="text-decoration: none; cursor: pointer; "'+
+					'class="glyphicon glyphicon-remove pull-right" '+
 				'ng-click="close()"></a></h3>'+
-		'</div>'+
-		'<div class="modal-body">'+
-			'{{message}}'+
-		'</div>' +
-		'<div class="modal-footer">'+
-			'<button type="button" class="btn btn-danger btn-xs" '+
-			'ng-click="close()">Cerrar</button>'+
-		'</div>',
-		size : 'sm',
-		controller : function($scope, $modalInstance, message, title)
-		{
-			$scope.message = message;
-
-			$scope.title = title;
-
-			$scope.close = function()
+			'</div>'+
+			'<div class="modal-body">'+
+				'{{message}}'+
+			'</div>' +
+			'<div class="modal-footer">'+
+				'<button type="button" class="btn btn-danger btn-xs" '+
+				'ng-click="close()">Cerrar</button>'+
+			'</div>',
+			size : 'sm',
+			controller : function($scope, $modalInstance, message, title)
 			{
-				$modalInstance.close();
-			};
-		}
-	};
+				$scope.message = message;
 
-	Popup.alert = function(message,title)
-	{
-		this.open('alert',message,title);
-	};
+				$scope.title = title;
 
-	Popup.open = function(type, message, title)
-	{
-		var resolves = 
-		{
-			resolve: 
-			{
-				title: function(){
-					return title;
-				},
-				message : function()
+				$scope.close = function()
 				{
-					return message;
-				}
+					$modalInstance.close();
+				};
 			}
 		};
 
-		angular.extend(options, resolves);
-
-		switch(type)
+		Popup.alert = function(message,title)
 		{
-			case ('alert'):
-				$modal.open(options);
-		}
-	};
+			this.open('alert',message,title);
+		};
 
-	return Popup;
+		Popup.open = function(type, message, title)
+		{
+			var resolves = 
+			{
+				resolve: 
+				{
+					title: function(){
+						return title;
+					},
+					message : function()
+					{
+						return message;
+					}
+				}
+			};
+
+			angular.extend(options, resolves);
+
+			switch(type)
+			{
+				case ('alert'):
+					$modal.open(options);
+			}
+		};
+
+		return Popup;
 	
-}]);
+	}]);
+});
